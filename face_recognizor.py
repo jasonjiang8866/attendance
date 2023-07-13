45e9f75110e8fdd5372b3a664931939d1de9bd08
Jason Jiang <jasonjiang8866@gmail.com>
2023-07-14 01:15:42 +0800

    dump pickle

--- face_recognizor.py
+++ face_recognizor.py
@@ -3,7 +3,7 @@ import os, sys
 import cv2
 import numpy as np
 import math
-
+import pickle
 
 # Helper
 def face_confidence(face_distance, face_match_threshold=0.6):
@@ -29,12 +29,19 @@ class FaceRecognition:
         self.encode_faces()
 
     def encode_faces(self):
+        self.known_face_encodings = []
+        self.known_face_names = []
         for image in os.listdir('faces'):
             face_image = face_recognition.load_image_file(f"faces/{image}")
             face_encoding = face_recognition.face_encodings(face_image)[0]
 
             self.known_face_encodings.append(face_encoding)
             self.known_face_names.append(image.split('.')[0])
+            
+            with open('known_face_encodings.dat', 'wb') as f:
+                pickle.dump(self.known_face_encodings, f)
+            with open('known_face_names.dat', 'wb') as f:
+                pickle.dump(self.known_face_names, f)
         # print(self.known_face_names)
 
 
